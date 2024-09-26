7f3dc39e8d48de20695431a1c20e47fb738b78a6
ArhipSizov <arhip.sizov111@gmail.com>
2024-09-26 19:41:20 +0300

    post_register

--- src/Pages/PostRegister/PostRegister.tsx
+++ src/Pages/PostRegister/PostRegister.tsx
@@ -36,8 +36,16 @@ export default function PostRegister() {
         <div className="post_register_1">
           <img className="top_img" src="/post_register/img1.png" alt="" />
           <div className="telephone">
-            <img className="telephone_11" src="/post_register/img01.png" alt="" />
-            <img className="telephone_12" src="/post_register/img91.png" alt="" />
+            <img
+              className="telephone_11"
+              src="/post_register/img01.png"
+              alt=""
+            />
+            <img
+              className="telephone_12"
+              src="/post_register/img91.png"
+              alt=""
+            />
           </div>
           <h1>Отслеживайте свои эмоции</h1>
           <p>
@@ -49,46 +57,54 @@ export default function PostRegister() {
       {showReg2 && (
         <div className="post_register_2">
           <img className="top_img" src="/post_register/img21.png" alt="" />
-          <img src="/post_register/img22.png" alt="" />
-          <div>
-            <img src="/post_register/img02.png" alt="" />
-            <img src="/post_register/img92.png" alt="" />
+          <img className="bottom_img" src="/post_register/img22.png" alt="" />
+          <div className="telephone">
+            <img
+              className="telephone_11"
+              src="/post_register/img02.png"
+              alt=""
+            />
+            <img
+              className="telephone_13"
+              src="/post_register/img92.png"
+              alt=""
+            />
           </div>
-          <h1>Отслеживайте свои эмоции</h1>
+          <h1>Терапия</h1>
           <p>
-            Отмечайте в дневнике ваше настроение, чем вы занимались
-            и что чувствовали в тот момент
+            С помощью разных видов терапии вы сможете улучшить свою психическую
+            устойчивость и благополучие. Также научитесь распознавать и решать
+            проблемы.
           </p>
         </div>
       )}
       {showReg3 && (
         <div className="post_register_3">
           <img className="top_img" src="/post_register/img3.png" alt="" />
-          <img src="/post_register/img03.png" alt="" />
-          <h1>Отслеживайте свои эмоции</h1>
+          <img className="telephone_14" src="/post_register/img03.png" alt="" />
+          <h1>Статистика</h1>
           <p>
-            Отмечайте в дневнике ваше настроение, чем вы занимались
-            и что чувствовали в тот момент
+            Покажет вам реальную картину вашего эмоционального состояния и
+            что приводит к возникновению тех или иных чувств.
           </p>
         </div>
       )}
       {showReg4 && (
         <div className="post_register_4">
           <img className="top_img" src="/post_register/img41.png" alt="" />
-          <img src="/post_register/img42.png" alt="" />
-          <h1>Отслеживайте свои эмоции</h1>
+          <img className="bottom_img" src="/post_register/img42.png" alt="" />
+          <h1>Советы</h1>
+          <p>Делайте записи регулярно.</p>
           <p>
-            Отмечайте в дневнике ваше настроение, чем вы занимались
-            и что чувствовали в тот момент
+            Относитесь непредвзято, пишите всё, что вы на самом деле чувствуете
+            и думаете.
+          </p>
+          <p>
+            В «Заметки» вы можете выплеснуть всё, что накопилось и использовать
+            как личный дневник.
           </p>
         </div>
       )}
-      <div className="footer_reg">
-        <div className=""></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
     </div>
   );
 }
--- src/Pages/Register/Register.tsx
+++ src/Pages/Register/Register.tsx
@@ -39,7 +39,7 @@ export default function Register() {
         displayName: null,
         email: null,
       }).then(() => {
-        navigate("/profile");
+        navigate("/post_register");
       });
     });
     addUserDB({ email, password });
